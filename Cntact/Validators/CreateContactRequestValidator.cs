aa5ed4d44a3a23b2acb0bb301132171e4dcc40ed
Kotlin4 <Vovazimov@yandex.ru>
2025-02-07 14:40:12 +0300

    v2

---
+++ Cntact/Validators/CreateContactRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using Cntact.Contracts;
+using FluentValidation;
+
+namespace Cntact.Validators
+{
+    public class CreateContactRequestValidator : AbstractValidator<CreateContactRequest>
+    {
+        public CreateContactRequestValidator() 
+        {
+            RuleFor(x => x.Number)
+                .NotEmpty().WithMessage("Номер телефона обязателен.")
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Некорректный номер телефона.");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Фамилия объязательна")
+                .MaximumLength(50).WithMessage("Максимальная длина фамилии - 50 символов.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Имя объязательно")
+                .MaximumLength(50).WithMessage("Максимальная длина имени - 50 символов.");
+
+
+        }
+    }
+}
---
+++ Cntact/Validators/DeleteContactRequestValidator.cs
@@ -0,0 +1,36 @@
+﻿using Cntact.Contracts;
+using FluentValidation;
+
+namespace Cntact.Validators
+{
+    public class DeleteContactRequestValidator : AbstractValidator<DeleteContactRequest>
+    {
+        public DeleteContactRequestValidator()
+        {
+            RuleFor(x => x)
+                .Must(request =>
+                    !string.IsNullOrWhiteSpace(request.Number) ||
+                    !string.IsNullOrWhiteSpace(request.FirstName) ||
+                    !string.IsNullOrWhiteSpace(request.Name) ||
+                    !string.IsNullOrWhiteSpace(request.LastName))
+                .WithMessage("Необходимо указать хотя бы одно поле для поиска.");
+
+            RuleFor(x => x.Number)
+                .Matches(@"^\+?[1-9]\d{1,14}$")
+                .When(x => !string.IsNullOrWhiteSpace(x.Number))
+                .WithMessage("Некорректный номер телефона.");
+
+            RuleFor(x => x.FirstName)
+                .MaximumLength(50).WithMessage("Имя не может быть длиннее 50 символов.")
+                .When(x => !string.IsNullOrWhiteSpace(x.FirstName));
+
+            RuleFor(x => x.Name)
+                .MaximumLength(50).WithMessage("Отчество не может быть длиннее 50 символов.")
+                .When(x => !string.IsNullOrWhiteSpace(x.Name));
+
+            RuleFor(x => x.LastName)
+                .MaximumLength(50).WithMessage("Фамилия не может быть длиннее 50 символов.")
+                .When(x => !string.IsNullOrWhiteSpace(x.LastName));
+        }
+    }
+}
